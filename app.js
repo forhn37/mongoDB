f0212a005ac72e672cdcca5063465f9ed8725f47
kimhyun <forhn37@gmail.com>
2023-11-02 16:41:43 +0900

    then catch 활용

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ const mongoose = require('mongoose'); //commonJS방식
 // import mongoose from 'mongoose'; //ESM방식
 // 둘다 가능
 
+mongoose.connect('mongodb://localhost/your-database-name', {
+  // mongoDB와 연결
+  // 첫번째 매개변수는 연결하려는 데이터베이스 URL
+  // 옵션객체 useNewUrlParser: MongoDB 연결 URL의 파싱을 위해 사용하는 옵션으로, true로 설정해야 합니다.
+  // useUnifiedTopology: 새로운 서버 디스커버 및 모니터링 엔진을 사용하는 옵션으로, true로 설정해야 합니다.
+  useNewUrlParser: true,
+  // 
+  useUnifiedTopology: true
+})
+.then(()=> {
+  console.log('MongoDB 연결 성공');
+})
+.catch((err)=> {
+  console.error('MongoDB 연결 실패:', err)
+});
