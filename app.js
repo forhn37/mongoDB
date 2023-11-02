f73769f93e74629baaeab86f03c732ddbe0a44b5
kimhyun <forhn37@gmail.com>
2023-11-02 16:52:30 +0900

    서버구성을 위한 PORT, app 변수선언

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose'); //commonJS방식
 // import mongoose from 'mongoose'; //ESM방식
 // 둘다 가능
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost/your-database-name', {
-  // mongoDB와 연결
+// MongoDB에 연결
+mongoose.connect('mongodb://localhost/mydb', {
   // 첫번째 매개변수는 연결하려는 데이터베이스 URL
   // 옵션객체 useNewUrlParser: MongoDB 연결 URL의 파싱을 위해 사용하는 옵션으로, true로 설정해야 합니다.
   // useUnifiedTopology: 새로운 서버 디스커버 및 모니터링 엔진을 사용하는 옵션으로, true로 설정해야 합니다.
@@ -12,3 +14,5 @@ mongoose.connect('mongodb://localhost/your-database-name', {
   // 
   useUnifiedTopology: true
 })
+
+// 
