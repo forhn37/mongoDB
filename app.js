ae2c6d46e9bbe4da3b8c8846a5135875ad21a09d
kimhyun <forhn37@gmail.com>
2023-11-02 18:20:32 +0900

    서버 가동 및 데이터베이스 설정 완료

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ mongoose.connect('mongodb://localhost/mydb', {
   // 첫번째 매개변수는 연결하려는 데이터베이스 URL
   // 옵션객체 useNewUrlParser: MongoDB 연결 URL의 파싱을 위해 사용하는 옵션으로, true로 설정해야 합니다.
   // useUnifiedTopology: 새로운 서버 디스커버 및 모니터링 엔진을 사용하는 옵션으로, true로 설정해야 합니다.
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true
 })
 
 // MongoDB 스키마 및 모델 정의
@@ -40,7 +40,7 @@ app.use(express.static('public'));
 
 // 루트 페이지
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 // POST 요청 처리
 app.post('/addData', async (req, res) => {
